a37410d8d08518e322a776a260bb9e03b0152bd5
Tom Oram <me@tomoram.io>
2019-05-07 21:29:11 +0100

    Introduce the Game monad

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts           #-}
+
 module TicTacToe (run) where 
 
 import Control.Error.Safe (headZ, readZ)
+import Control.Monad.State.Strict (MonadState, StateT, evalStateT, get, put, modify)
 
 import           TicTacToe.Board (Board, Cell(..))
 import qualified TicTacToe.Board as Board
@@ -10,35 +13,73 @@ import qualified TicTacToe.Player as Player
 import           TicTacToe.GameLogic (GameState(..))
 import qualified TicTacToe.GameLogic as GameLogic
 
+data TheState = TheState { theBoard :: Board
+                         , currentPlayer :: Player
+                         }
+
+newState :: TheState
+newState = TheState { theBoard = Board.new, currentPlayer = Crosses }
+
+newtype Game m a = Game { runGame :: StateT TheState m a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadState TheState)
+
+class Monad m => Actions m where
+  player       :: m Player
+  board        :: m Board
+  switchPlayer :: m ()
+  setCell      :: Int -> m ()
+
+cell :: Player -> Cell
+cell Naughts = Naught
+cell Crosses = Cross
+
+instance Monad m => Actions (Game m) where
+  player = currentPlayer <$> get
+  board = theBoard <$> get
+  switchPlayer = modify (\s -> s { currentPlayer = Player.switch (currentPlayer s) })
+  setCell position = do
+    p <- player
+    modify (\s -> s { theBoard = Board.setCell (theBoard s) (cell p) position })
+
 play :: Board -> Player -> Int -> Board
 play board Naughts position = Board.setCell board Naught position
 play board Crosses position = Board.setCell board Cross position
 
-drawBoard :: Board -> IO ()
+drawBoard :: MonadIO m => Board -> m ()
 drawBoard = putStrLn . tshow
 
-gameOver :: Board -> Text -> IO ()
+gameOver :: MonadIO m =>  Board -> Text -> m ()
 gameOver board message = do
   drawBoard board
   putStr "Game over: "
   putStrLn message
 
-evaluateState :: Board -> Player -> IO ()
-evaluateState board player = case state of
-    InPlay -> gameLoop board $ Player.switch player
-    _      -> gameOver board (tshow state)
-  where state = GameLogic.getGameState board
+evaluateState :: Game IO ()
+evaluateState = do
+  b <- board
+  let state = GameLogic.getGameState b
+  case state of
+    InPlay -> switchPlayer >> gameLoop
+    _      -> gameOver b (tshow state)
 
-gameLoop :: Board -> Player -> IO ()
-gameLoop state player = do
+gameLoop :: Game IO ()
+gameLoop = do
+    state <- board
+    player <- player
     drawBoard state
     putStr (tshow player)
     putStr ", "
     putStrLn "choose cell: "
-    (Just position) <- readZ . unpack <$> getLine
-    evaluateState (play state player $ position - 1)  player 
+    posMay <- readZ . unpack <$> getLine
+    case posMay of
+      Just position -> do setCell (position - 1)
+                          evaluateState
+      Nothing       -> void $ putStrLn "Fail"
+
+run' :: Game IO ()
+run' = gameLoop
 
 run :: IO ()
 run = do
     putStrLn "Tic Tac Toe"
-    gameLoop Board.new Crosses
+    evalStateT (runGame run') newState
