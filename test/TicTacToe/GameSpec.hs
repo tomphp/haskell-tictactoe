ea88f38f4efc332a914e534cc25cb726f9df4c9d
Tom Oram <me@tomoram.io>
2019-05-21 22:05:42 +0100

    Remove commented code

--- test/TicTacToe/GameSpec.hs
+++ test/TicTacToe/GameSpec.hs
@@ -148,33 +148,3 @@ spec = do
     describe "gameOverScreen" $ do
       it "something" $ do
         pending
-
-    -- context "getGameState" $ do
-    --   it "is in play if there are empty cells and no winning lines" $ do
-    --     let board = Board [ Naught, Cross,  Empty
-    --                       , Naught, Cross,  Cross
-    --                       , Cross,  Naught, Naught
-    --                       ]
-    --     GameLogic.getGameState board `shouldBe` InPlay
-
-    --   it "is a draw if all cells are taken and there are no winning lines" $ do
-    --     let board = Board [ Naught, Cross,  Naught
-    --                       , Naught, Cross,  Cross
-    --                       , Cross,  Naught, Naught
-    --                       ]
-    --     GameLogic.getGameState board `shouldBe` Draw
-
-    --   it "has been won be crosses if the there is a winning line of crosses" $ do
-    --     let board = Board [ Naught, Cross,  Naught
-    --                       , Cross,  Cross,  Cross
-    --                       , Empty,  Naught, Naught
-    --                       ]
-    --     GameLogic.getGameState board `shouldBe` Winner Crosses
-         
-
-    --   it "has been won be naughts if the there is a winning line of naughts" $ do
-    --     let board = Board [ Naught, Cross,  Naught
-    --                       , Cross,  Naught, Cross
-    --                       , Empty,  Naught, Naught
-    --                       ]
-    --     GameLogic.getGameState board `shouldBe` Winner Naughts
