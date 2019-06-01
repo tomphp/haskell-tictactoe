c2f5c0b410f02e7830cbb151da999c25da14b23a
Tom Oram <me@tomoram.io>
2019-06-01 19:15:11 +0100

    Refactor Line.winner

--- src/TicTacToe/Line.hs
+++ src/TicTacToe/Line.hs
@@ -3,13 +3,12 @@ module TicTacToe.Line
   , winner
   ) where
 
-import Control.Error.Safe (headZ)
 import Data.List (nub)
 
 newtype Line a = Line [Maybe a] deriving (Eq, Show)
 
 winner :: Eq a => Line a -> Maybe a
-winner (Line cs)
-  | uniqueCells /= 1 = Nothing
-  | otherwise        = join $ headZ cs
-  where uniqueCells = length $ nub cs
+winner (Line cells) =
+  case nub cells of
+    [cell] -> cell
+    _      -> Nothing
